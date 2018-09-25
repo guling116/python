ab594ab8f195d2d05a955c8b2ad2de034e17762d
root <root@localhost.localdomain>
2018-09-25 04:03:33 -0400

    day01.py

--- day01.py
+++ day01.py
@@ -1,1 +1,8 @@
-print("hello word")
+
+time_ = int(raw_input(">>"))
+min_ = time_ // 60
+times = time_ % 60
+print(min_ , times)
+'''x,y,a,b,c=1,1,0,0,0
+print((3+4*x)/5-10*(y-5)*(a+b+c)/x+9*(4/x+(9+x/y)))'''
+
