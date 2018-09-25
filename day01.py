74ca1231d31b610d6f1d69231f3d585a308cdf69
root <root@localhost.localdomain>
2018-09-25 05:31:33 -0400

    day01.py

--- day01.py
+++ day01.py
@@ -1,8 +1,61 @@
-
+'''
 time_ = int(raw_input(">>"))
 min_ = time_ // 60
 times = time_ % 60
 print(min_ , times)
-'''x,y,a,b,c=1,1,0,0,0
+x,y,a,b,c=1,1,0,0,0
 print((3+4*x)/5-10*(y-5)*(a+b+c)/x+9*(4/x+(9+x/y)))'''
+
+
+#EP-1
+#celsius =float(input("shu ru yi ge Celsius>>"))
+#fahrenheit = (9/5.0)*celsius+32
+#print("fahrenheit is " ,fahrenheit)
+'''
+#EP-2
+banjing = float(input("shu ru yi ge ban jing>>"))
+height = float(input("shu ru gao>>"))
+area=banjing * banjing * 3.14
+volume=area * height
+print("The area",area)
+print("The volume",volume)
+
+#EP-2-2
+banjing,height=eval(input("shu ru banjing he gao>>"))
+area=banjing * banjing * 3.14
+volume=area * height
+print(area,volume)
+'''
+
+'''
+#EP-3
+yingchi=float(input("chu ru yi ge yingchi>>"))
+m=yingchi * 0.305 
+print(m,"zhe me duo mi")
+'''
+
+
+'''
+#EP-4
+M,initial,final=eval(input("water KG , qi shi wen du ,zui zhong wen du >>"))
+Q=M*(final-initial) * 4184
+print('The energy needed is {}'.format(Q))
+'''
+'''
+#Ep-5
+chae,nianlilv=eval(input("Enter chae and nianlilv rate(e.g.,3for3%:>>) "))
+interest=chae * (nianlilv/1200)
+print('The interest is {}'.format(interest))
+'''
+'''
+#EP-6
+v0,v1,t=eval(input("Enter v0 v1 and t:>>"))
+a=(v1-v0)/t
+print('The average acceleration is {}'.format(a))
+'''
+
+
+
+
+
 
