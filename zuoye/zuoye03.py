8882b0f071a84c2e01700904ef5b342c3ec1b400
root <root@localhost.localdomain>
2018-10-09 04:36:00 -0400

    zuoye03.py

--- zuoye/zuoye03.py
+++ zuoye/zuoye03.py
@@ -143,47 +143,32 @@ for i in range(1,100000):
     if(i%10000==0):
         print(4*n)
 '''
+'''
+#3-10
 
-#_*_coding:utf8-
-import platform
-import os
-import time
-import _thread
-
-def get_os():
-	os = platform.system()
-	if os == 'Windows':
-		return 'n'
-	else:
-		return 'c'
-		
-def ping_ip(ip_str):
-	cmd = ['ping','-{op}'.format(op=get_os()),'1',ip_str]
-	
-	output = os.popen(' '.join(cmd)).readlines()
-	
-	for line in list(output):
-		if not line:
-			continue
-		if str(line).upper().find('TTL') >= 0:
-			print('%s on' % ip_str)
-			break
-			
-			
-def find_ip(ip_prefix):
-	for i in range(1,256):
-		ip = '%s.%s' % (ip_prefix,i)
-		_thread.start_new_thread(ping_ip,(ip,))
-		time.sleep(0.3)
+for i in range(1,10001):
+    n=0
+    for j in range(1,i/2+1):
+        if(i%j==0):
+            n=n+j
+    if(n==i):
+        print(i)
+'''
 
 
-if __name__ == '__main__':
-	start_time = time.clock()
-	ip_prefix = '192.168.3'
-	print('%sruning' % ip_prefix)
-	find_ip(ip_prefix)
-	print('over')
-	
+'''
+#3-11
+a=0
+for i in range(1,8):
+    for j in range(i+1,8):
+        print("sum {}".format((i)+(j)))
+        a=a+1
+print "sum",a
+'''
+
+
+
+
 
 
 
